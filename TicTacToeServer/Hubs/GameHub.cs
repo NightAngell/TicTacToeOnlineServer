579a26d5537254b5852c4dae6fc5934fd9d955cd
NightAngell <sobta24@wp.pl>
2018-11-26 23:10:11 +0100

    Created working game multiplayer prototype

--- TicTacToeServer/Hubs/GameHub.cs
+++ TicTacToeServer/Hubs/GameHub.cs
@@ -8,5 +8,30 @@ namespace TicTacToeServer.Hubs
 {
     public class GameHub : Hub
     {
+        public async Task JoinToGame(int gameId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, _createGroupName(gameId));
+            Context.Items.Add("gameId", gameId);
+            await Clients.Caller.SendAsync("JoinedToGame");
+        }
+
+        public async Task Ping(int gameId)
+        {
+            await Clients.OthersInGroup(_createGroupName(gameId)).SendAsync("OpponentConnected");
+        }
+
+        public async Task MakeMove(int i, int j)
+        {
+            await Clients
+                .OthersInGroup(_createGroupName((int)Context.Items["gameId"]))
+                .SendAsync("OpponentMadeMove", i, j);
+        }
+
+        private string _createGroupName(int gameId)
+        {
+            var now = DateTime.Now;
+            var groupName = $"{now.Year}/{gameId}";
+            return groupName;
+        }
     }
 }
