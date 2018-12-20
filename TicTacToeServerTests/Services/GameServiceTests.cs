21a2becd227df5cbbe60b54d0db9014a946874bd
NightAngell <sobta24@wp.pl>
2018-12-20 14:23:46 +0100

    Added gameService.IsWinner tests

--- TicTacToeServerTests/Services/GameServiceTests.cs
+++ TicTacToeServerTests/Services/GameServiceTests.cs
@@ -25,6 +25,105 @@ namespace TicTacToeServerTests.Services
             };
         }
 
+        #region gameFieldsHorizontalWinner
+        private static GameField _gameFieldWinnerHorizontalTop()
+        {
+            return new GameField {
+                TopLeft = "x",
+                Top = "x",
+                TopRight = "x"
+            };
+        }
+
+        private static GameField _gameFieldWinnerHorizontalMiddle()
+        {
+            return new GameField
+            {
+                MiddleLeft = "x",
+                Middle = "x",
+                MiddleRight = "x"
+            };
+        }
+
+        private static GameField _gameFieldWinnerHorizontalDown()
+        {
+            return new GameField
+            {
+                DownLeft = "x",
+                Down = "x",
+                DownRight = "x"
+            };
+        }
+        #endregion
+
+        #region gameFieldsVerticalWinner
+        private static GameField _gameFieldWinnerVerticalLeft()
+        {
+            return new GameField
+            {
+                DownLeft = "x",
+                MiddleLeft = "x",
+                TopLeft = "x"
+            };
+        }
+
+        private static GameField _gameFieldWinnerVerticalMiddle()
+        {
+            return new GameField
+            {
+                Top = "x",
+                Middle = "x",
+                Down = "x"
+            };
+        }
+
+        private static GameField _gameFieldWinnerVerticalRight()
+        {
+            return new GameField
+            {
+                TopRight = "x",
+                MiddleRight = "x",
+                DownRight = "x"
+            };
+        }
+        #endregion
+
+        #region gameFieldsUnderTheSlantWinner
+        private static GameField _gameFieldTopLeftDownRightWinner()
+        {
+            return new GameField
+            {
+                TopLeft = "x",
+                Middle = "x",
+                DownRight = "x"
+            };
+        }
+
+        private static GameField _gameFieldDownLeftTopRightWinner()
+        {
+            return new GameField
+            {
+                DownLeft = "x",
+                Middle = "x",
+                TopRight = "x"
+            };
+        }
+        #endregion
+
+        private static object[] _gameFieldsWhereIsWinner =
+        {
+            new object[] { _gameFieldWinnerHorizontalTop() },
+            new object[] { _gameFieldWinnerHorizontalMiddle() },
+            new object[] { _gameFieldWinnerHorizontalDown() },
+
+            new object[] { _gameFieldWinnerVerticalLeft() },
+            new object[] { _gameFieldWinnerVerticalMiddle() },
+            new object[] { _gameFieldWinnerVerticalRight() },
+
+            new object[] { _gameFieldTopLeftDownRightWinner() },
+            new object[] { _gameFieldDownLeftTopRightWinner() }
+        };
+       
         [SetUp]
         public void SetUp()
         {
@@ -33,6 +132,7 @@ namespace TicTacToeServerTests.Services
             _gameService = new GameService(_roomServiceMock.Object);
         }
 
+        //ValidatePlayer
         [Test]
         public void ValidatePlayer_PlayersWithValidDataExist_ReturnsTrue()
         {
@@ -93,6 +193,44 @@ namespace TicTacToeServerTests.Services
            );
         }
 
+        //IsWinner
+        [Test]
+        public void IsWinner_FieldIsEmpty_ReturnsFalse()
+        {
+            var emptyField = new GameField();
+            Assert.IsFalse(_gameService.IsWinner(emptyField));
+        }
 
+        [Test, TestCaseSource(nameof(_gameFieldsWhereIsWinner))]
+        public void IsWinner_IsWinner_ReturnsTrue(GameField field)
+        {
+            Assert.IsTrue(_gameService.IsWinner(field));
+        }
+
+        [Test]
+        public void IsWinner_PartOfFieldsNotEmpty_ReturnsFalse()
+        {
+            var field = new GameField {
+                TopLeft = "x",
+                TopRight = "x",
+                Down = "o",
+                DownLeft = "o"
+            };
+
+            Assert.IsFalse(_gameService.IsWinner(field));
+        }
+
+        [Test]
+        public void IsWinner_XonTopAndBottomOMiddle_ReturnsFalse()
+        {
+            var field = new GameField
+            {
+                Top = "x",
+                Middle = "0",
+                Down = "x",
+            };
+
+            Assert.IsFalse(_gameService.IsWinner(field));
+        }
     }
 }
