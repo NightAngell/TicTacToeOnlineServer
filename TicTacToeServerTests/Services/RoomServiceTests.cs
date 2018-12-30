90dec1be8b3a2dd67ab509d7bb733efec29238b4
NightAngell <sobta24@wp.pl>
2018-12-30 03:54:00 +0100

    Added Sqllite in memory db for testing purpose

---
+++ TicTacToeServerTests/Services/RoomServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestSupport.EfHelpers;
+using TicTacToeServer.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TicTacToeServer.Models;
+
+namespace TicTacToeServerTests.Services
+{
+    /// <summary>
+    /// https://github.com/JonPSmith/EfCore.TestSupport/wiki/1.-Sqlite-in-memory-test-database
+    /// </summary>
+    [TestFixture]
+    class RoomServiceTests
+    {
+        [Test]
+        public void Test()
+        {
+            using (var context = new Db(SqliteInMemory.CreateOptions<Db>()))
+            {
+                context.Database.EnsureCreated();
+                context.Rooms.Add(new Room() { HostNick="hn"});
+                context.SaveChanges();
+                Assert.IsTrue(context.Rooms.Count() == 1);
+            }
+        }
+    }
+}
