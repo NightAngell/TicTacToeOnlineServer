a6e0d2d4f2ebc3f190dd06d2ab843d36e673df2a
NightAngell <sobta24@wp.pl>
2019-01-19 21:45:19 +0100

    Created first selenium test

---
+++ TicTacToeSeleniumTests/Auth/LoginTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToeSeleniumTests.PageObjects;
+
+namespace TicTacToeSeleniumTests.Auth
+{
+    [TestFixture]
+    class LoginTests
+    {
+        FirefoxDriver _firefoxDriver = new FirefoxDriver(Environment.CurrentDirectory);
+
+        [Test]
+        public void WeHaveValidLoginAndPassword_UserLoggedIn()
+        {
+            var loginPage = new LoginPage(_firefoxDriver);
+            loginPage.Navigate();
+            loginPage.LoginInput.SendKeys("sobta24@wp.pl");
+            loginPage.PasswordInput.SendKeys("qwerty123");
+            loginPage.SubmitButton.Submit();
+
+            //ClearAfterTest
+            _firefoxDriver.FindElementByClassName("logout").Click();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           _firefoxDriver.Close();
+        }
+    }
+}
---
+++ TicTacToeSeleniumTests/PageObjects/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeSeleniumTests.PageObjects
+{
+
+    class LoginPage
+    {
+        protected readonly IWebDriver _driver;
+        protected const string loginCssSelector = "app-loginandregistration > form > input[name='email']";
+        protected const string passwordCssSelector = "app-loginandregistration > form > input[name='password']";
+        protected const string submitButtonCssSelector = "app-loginandregistration > form > input[type='submit']";
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = loginCssSelector)]
+        public IWebElement LoginInput { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = passwordCssSelector)]
+        public IWebElement PasswordInput { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = submitButtonCssSelector)]
+        public IWebElement SubmitButton { get; set; }
+
+        public void Navigate()
+        {
+            _driver
+                .Navigate()
+                .GoToUrl("http://localhost:4200/login");
+        }
+    }
+}
