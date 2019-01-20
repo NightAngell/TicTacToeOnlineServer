812516f7fb22ea1ae304735bda3d6a79091dbdb4
NightAngell <sobta24@wp.pl>
2019-01-20 14:43:05 +0100

    Added more selenium tests for Login page

--- TicTacToeSeleniumTests/Auth/LoginTests.cs
+++ TicTacToeSeleniumTests/Auth/LoginTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -12,25 +14,92 @@ namespace TicTacToeSeleniumTests.Auth
     [TestFixture]
     class LoginTests
     {
-        FirefoxDriver _firefoxDriver = new FirefoxDriver(Environment.CurrentDirectory);
+        private RemoteWebDriver _getRequiredDriver()
+        {
+            return new FirefoxDriver(Environment.CurrentDirectory);
+        }
 
         [Test]
         public void WeHaveValidLoginAndPassword_UserLoggedIn()
         {
-            var loginPage = new LoginPage(_firefoxDriver);
-            loginPage.Navigate();
-            loginPage.LoginInput.SendKeys("sobta24@wp.pl");
-            loginPage.PasswordInput.SendKeys("qwerty123");
-            loginPage.SubmitButton.Submit();
+            using (var driver = _getRequiredDriver())
+            {
+                var loginPage = new LoginPage(driver);
+                loginPage.Navigate();
+                loginPage.LoginInput.SendKeys(LoginAndPassword.Login);
+                loginPage.PasswordInput.SendKeys(LoginAndPassword.Password);
+                loginPage.SubmitButton.Submit();
 
-            //ClearAfterTest
-            _firefoxDriver.FindElementByClassName("logout").Click();
+                //Check it logout button exist, If yes, user is logged in
+                new WebDriverWait(driver, TimeSpan.FromSeconds(40))
+                    .Until(ExpectedConditions.ElementExists(By.CssSelector(".logout")));
+
+                //Clear after test
+                driver.FindElementByClassName("logout").Click();
+            }
         }
 
-        [TearDown]
-        public void TearDown()
+        [Test]
+        public void WeHaveInvalidLogin_UserSeeEmailInvalidInfoDiv()
         {
-           _firefoxDriver.Close();
+            using (var driver = _getRequiredDriver())
+            {
+                var loginPage = new LoginPage(driver);
+                loginPage.Navigate();
+                loginPage.LoginInput.SendKeys("invalidemail.com");
+                loginPage.PasswordInput.Click();
+
+                Assert.IsTrue(loginPage.EmailInvalidInfoDiv.Displayed);
+            }
+        }
+
+        [Test]
+        public void WeHaveTooShortPassword_UserSeePasswordInvalidInfoDiv()
+        {
+            using (var driver = _getRequiredDriver())
+            {
+                var loginPage = new LoginPage(driver);
+                loginPage.Navigate();
+                loginPage.PasswordInput.Click();
+                loginPage.LoginInput.Click();
+
+                Assert.IsTrue(loginPage.PasswordInvalidInfoDiv.Displayed);
+            }
+        }
+
+        [Test]
+        public void LoginIsValidEmailButPasswordIsInvalidInAccounContext_UserSeeWrongLoginOrPasswordDiv()
+        {
+            using (var driver = _getRequiredDriver())
+            {
+                var loginPage = new LoginPage(driver);
+                loginPage.Navigate();
+                loginPage.PasswordInput.SendKeys($"{LoginAndPassword.Password}a");
+                loginPage.LoginInput.SendKeys(LoginAndPassword.Login);
+                loginPage.SubmitButton.Submit();
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(40))
+                    .Until(ExpectedConditions.ElementExists(
+                        By.CssSelector(LoginPage.wrongLoginOrPasswordInfoDivSelector))
+                    );
+
+                Assert.IsTrue(loginPage.WrongLoginOrPasswordDiv.Displayed);
+            } 
+        }
+
+        [Test]
+        public void UserClickToRedirectToRegistrationButton_UserRedirectedToRegistrationPage()
+        {
+            using (var driver = _getRequiredDriver())
+            {
+                var loginPage = new LoginPage(driver);
+                loginPage.Navigate();
+                loginPage.RedirectToRegistrationDiv.Click();
+
+                //Check if user is redirected to registration page
+                new WebDriverWait(driver, TimeSpan.FromSeconds(40))
+                    .Until(ExpectedConditions.UrlContains("registration"));
+            }
         }
     }
 }
--- TicTacToeSeleniumTests/PageObjects/LoginPage.cs
+++ TicTacToeSeleniumTests/PageObjects/LoginPage.cs
@@ -10,9 +10,13 @@ namespace TicTacToeSeleniumTests.PageObjects
     class LoginPage
     {
         protected readonly IWebDriver _driver;
-        protected const string loginCssSelector = "app-loginandregistration > form > input[name='email']";
-        protected const string passwordCssSelector = "app-loginandregistration > form > input[name='password']";
-        protected const string submitButtonCssSelector = "app-loginandregistration > form > input[type='submit']";
+        public const string loginCssSelector = "app-loginandregistration > form > input[name='email']";
+        public const string passwordCssSelector = "app-loginandregistration > form > input[name='password']";
+        public const string submitButtonCssSelector = "app-loginandregistration > form > input[type='submit']";
+        public const string emailInvalidInforDivSelector = "div[emailInvalidInfo]";
+        public const string passwordInfalidInfoForDivSelector = "div[passwordInvalidInfo]";
+        public const string wrongLoginOrPasswordInfoDivSelector = "div[wrongLoginOrPasswordInfo]";
+        public const string redirectLinkDivSelector = "div[routerLink='/registration']";
 
         public LoginPage(IWebDriver driver)
         {
@@ -28,6 +32,18 @@ namespace TicTacToeSeleniumTests.PageObjects
 
         [FindsBy(How = How.CssSelector, Using = submitButtonCssSelector)]
         public IWebElement SubmitButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = emailInvalidInforDivSelector)]
+        public IWebElement EmailInvalidInfoDiv { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = passwordInfalidInfoForDivSelector)]
+        public IWebElement PasswordInvalidInfoDiv { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = wrongLoginOrPasswordInfoDivSelector)]
+        public IWebElement WrongLoginOrPasswordDiv { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = redirectLinkDivSelector)]
+        public IWebElement RedirectToRegistrationDiv { get; set; }
 
         public void Navigate()
         {
