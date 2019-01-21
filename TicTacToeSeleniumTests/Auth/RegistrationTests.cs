85a74c3a9853889b2fa1eb53db64f70be77fa5c2
NightAngell <sobta24@wp.pl>
2019-01-21 20:31:30 +0100

    Added selenium registration tests

--- TicTacToeSeleniumTests/Auth/RegistrationTests.cs
+++ TicTacToeSeleniumTests/Auth/RegistrationTests.cs
@@ -45,5 +45,48 @@ namespace TicTacToeSeleniumTests.Auth
                 }
             }
         }
+
+        [Test]
+        public void UserTypeInvalidEmailIntoInput_UserSeeInvalidEmailErrorAndSubmitButtonIsDisabled()
+        {
+            using (var driver = _getNewInstanceOfRequiredDriver())
+            {
+                var registrationPage = new RegistrationPage(driver);
+                registrationPage.Navigate();
+                registrationPage.LoginInput.SendKeys("invalidEmail.com");
+                registrationPage.PasswordInput.Click();
+
+                Assert.IsTrue(registrationPage.EmailInvalidInfoDiv.Displayed);
+                Assert.IsTrue(!registrationPage.SubmitButton.Enabled);
+            }
+        }
+
+        [Test]
+        public void UserTypeInvalidPasswordIntoInput_UserSeeInvaLidErrorAndSubmitButtonIsDisabled()
+        {
+            using (var driver = _getNewInstanceOfRequiredDriver())
+            {
+                var registrationPage = new RegistrationPage(driver);
+                registrationPage.Navigate();
+                registrationPage.PasswordInput.Click();
+                registrationPage.LoginInput.Click();
+                
+                Assert.IsTrue(registrationPage.PasswordInvalidInfoDiv.Displayed);
+                Assert.IsTrue(!registrationPage.SubmitButton.Enabled);
+            }
+        }
+
+        [Test]
+        public void UserClickToRedirectToLoginButton_UserRedirectedToLoginPage()
+        {
+            using (var driver = _getNewInstanceOfRequiredDriver())
+            {
+                var loginPage = new RegistrationPage(driver);
+                loginPage.Navigate();
+                loginPage.RedirectToLoginDiv.Click();
+
+                _verifyUserIsRedirected("login", driver);
+            }
+        }
     }
 }
