6f087461c1f218a9ff79878e280f0fd99330433d
NightAngell <sobta24@wp.pl>
2019-01-22 01:59:11 +0100

    Added some lobby tests

---
+++ TicTacToeSeleniumTests/PageObjects/LobbyPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumExtras.PageObjects;
+
+namespace TicTacToeSeleniumTests.PageObjects
+{
+    class LobbyPage : PageObjectBase
+    {
+        public const string modalContentClassName = "modalContent";
+
+        /// <summary>
+        /// This element exist only if room is hosted in lobby
+        /// </summary>
+        public const string firstRoomInLobbySelector = "app-lobby > div > div.bottomLobby.flex-col-9.flex-container.flex-row > div.listOfRooms.flex-col-9 > div";
+
+        public LobbyPage(RemoteWebDriver driver) 
+            : base(driver, "lobby")
+        {
+        }
+
+        [FindsBy(How = How.ClassName, Using = "playerNickInput")]
+        public IWebElement PlayerNickInput { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "paswordInput")]
+        public IWebElement PasswordInput { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "hostButton")]
+        public IWebElement HostButton { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "refreshButton")]
+        public IWebElement RefreshButton { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "menuButton")]
+        public IWebElement MenuButton { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "listOfRooms")]
+        public IWebElement ListOfRooms { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = modalContentClassName)]
+        public IWebElement ModalContent { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "abortButton")]
+        public IWebElement ModalAbortButton { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "modalHostedRoomId")]
+        public IWebElement ModalHostedRoomId { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "modalHostNick")]
+        public IWebElement ModalHostNick { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "modalHostedRoomPassword")]
+        public IWebElement ModalHostedRoomPassword { get; set; }
+
+        /// <summary>
+        /// This element exist only if room is hosted in lobby
+        /// </summary>
+        [FindsBy(How = How.CssSelector, Using = firstRoomInLobbySelector)]
+        public IWebElement FirstRoom { get; set; }
+
+        
+    }
+}
---
+++ TicTacToeSeleniumTests/Tests/LobbyTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicTacToeSeleniumTests.PageObjects;
+
+namespace TicTacToeSeleniumTests.Tests
+{
+    [TestFixture]
+    class LobbyTests : SeleniumWithWorkingAuthBase
+    {
+        [Test]
+        public void UserClickMenuButton_UserIsRedirectedToMenu()
+        {
+            using (var driver = _getNewInstanceOfRequiredDriver())
+            {
+                LogIn(driver);
+
+                var lobbyPage = new LobbyPage(driver);
+                lobbyPage.Navigate();
+
+                lobbyPage.MenuButton.Click();
+
+                _verifyUserIsRedirected("", driver);
+
+                LogOut(driver);
+            }
+        }
+
+        [Test]
+        public void UserHaveNickAndPasswordThenClickHostButtonThenClickAbortRoomButton_UserCanSeeRoomIdAndNickAndPasswordThenRoomIsAborted()
+        {
+            using (var driver = _getNewInstanceOfRequiredDriver())
+            {
+                LogIn(driver);
+
+                var lobbyPage = new LobbyPage(driver);
+                lobbyPage.Navigate();
+
+                lobbyPage.PlayerNickInput.SendKeys("nick");
+                lobbyPage.PasswordInput.SendKeys("password");
+                lobbyPage.HostButton.Click();
+
+                _waitForElement(driver, By.ClassName(LobbyPage.modalContentClassName));
+
+                Assert.Greater(lobbyPage.ModalHostedRoomId.Text.Length, 0);
+                Assert.AreEqual("nick", lobbyPage.ModalHostNick.Text);
+                Assert.AreEqual("password", lobbyPage.ModalHostedRoomPassword.Text);
+
+                lobbyPage.ModalAbortButton.Click();
+
+                _waitForElement(driver, By.ClassName("logout"));
+                LogOut(driver);
+            }
+        }
+
+        [Test]
+        public void WeHaveHostAndGuestUsersWhichTryStartGame_UsersRedirectedToMultiplayerGame()
+        {
+            using (var hostDriver = _getNewInstanceOfRequiredDriver())
+            using (var guestDriver = _getNewInstanceOfRequiredDriver())
+            {
+                LogIn(hostDriver);
+                LogIn(guestDriver);
+
+                var hostLobbyPage = new LobbyPage(hostDriver);
+                hostLobbyPage.Navigate();
+                var guestLobbyPage = new LobbyPage(guestDriver);
+                guestLobbyPage.Navigate();
+
+                hostLobbyPage.HostButton.Click();
+                _waitForElement(hostDriver, By.ClassName(LobbyPage.modalContentClassName));
+
+                guestLobbyPage.RefreshButton.Click();
+                _waitForElement(guestDriver, By.CssSelector(LobbyPage.firstRoomInLobbySelector));
+                guestLobbyPage.FirstRoom.Click();
+
+                _verifyUserIsRedirected("multiplayerGame", hostDriver);
+                _verifyUserIsRedirected("multiplayerGame", guestDriver);
+
+                hostLobbyPage.Navigate();
+                LogOut(hostDriver);
+                guestLobbyPage.Navigate();
+                LogOut(guestDriver);
+            }
+        }
+    }
+}
